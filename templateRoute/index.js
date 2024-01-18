c981734593920942ad11f844b4e3fcda371f51b1
Giovanni Troisi <giovanni@tiledesk.com>
2024-01-18 10:43:49 +0100

    manually merge with Mirco edits from tiledesk-community-app repo

--- templateRoute/index.js
+++ templateRoute/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Faq_kb = require('./models/faq_kb');
-const faqService = require('./models/faqService');
-const faqKbService = require('./models/faqKbService');
+const faqService = require('./services/faqService');
+const faqKbService = require('./services/faqKbService');
 
 let log = false;
 
@@ -25,22 +25,49 @@ router.get('/', (req, res) => {
 //   }
 // });
 
+//RETURN THE SEARCH FULLTEXT OR FOR LANGUAGE AND CATEGORY
 router.get('/public/community', async (req, res) => { // ?text=...
+  console.log("/public/community")
+  // full-text
   let text = req.query.text;
+  // language chatbot
+  let lang = req.query.lang;
+  //mainCategory chatbot
+  let category = req.query.category;
   let bots = [];
-  let query = {public: true, certified: false, "trashed": { $in: [null, false] }};
+  let query = { public: true, "trashed": { $in: [null, false] } };
+  //let query = { public: true, "trashed": { $in: [null, false] }, language: req.query.language };
+  //query.language = req.query.language;
   // var query = { "id_project": req.projectid, "trashed": { $in: [null, false] } };
-  let search_obj = {"$search": text};
 
-  if (text) {    
-    // if (req.query.language) {
-    //   search_obj["$language"] = req.query.language;
-    // }
-    query.$text = search_obj; 
-    console.log("Using query:", query)   
+  // CONTROLL THE LANGUAGE SELECTED FROM THE USER
+  console.log('lang ', lang);
+  if (lang && lang != 'undefined' && lang != 'all_lang') {
+    console.log("lang length", lang.split('-').length);
+    if (lang.split('-').length > 1) {
+      const lang1 = lang.split('-')[0];
+      console.log("before -:", lang1);
+      const lang2 = lang.split('-')[1];
+      console.log("after -:", lang2);
+      query.language = { '$in': [lang1, lang2] };
+    } else {
+      query.language = { '$in': [lang] };
+    }
   }
-
+  // CONTROLL THE CATEGORY SELECTED FROM THE USER
+  console.log('category ', category);
+  if (category && category != 'undefined' && category != 'all_cat') {
+    query.mainCategory = { '$in': [category] };
+  }
+  // CONTROLL THE FULL TEXT SEARCH 
+  console.log('text ', text);
+  let search_obj = { "$search": text };
+  if (text && text != 'undefined') {
+    query.$text = search_obj;
+    console.log("Using query:", query);
+  }
   try {
+    console.log("Using query:", query);
     bots = await faqKbService.getAll(query);
     console.log("bots found:", bots.length);
     res.send(bots);
@@ -51,9 +78,26 @@ router.get('/public/community', async (req, res) => { // ?text=...
   }
 });
 
+router.get('/public/author/:authoid', async (req, res) => {
+  // author chatbot
+  let authorid = req.params.authoid;
+  console.log("authorID:", authorid);
+  let bots = [];
+  let query = { 'public': true, trashed: { $in: [null, false] }, createdBy: { $eq: authorid } };
+  try {
+    console.log("Using author query:", query);
+    bots = await faqKbService.getAll(query);
+    res.send(bots);
+  }
+  catch (err) {
+    console.error('GET FAQ-KBs ERROR ', err);
+    return res.status(500).send({ success: false, msg: 'Error getting bots.' });
+  }
+});
+
 router.get('/public/templates', async (req, res) => {
   let bots = [];
-  let query = {public: true, certified: true, "trashed": { $in: [null, false] }};
+  let query = { public: true, certified: true, "trashed": { $in: [null, false] } };
   try {
     bots = await faqKbService.getAll(query);
     res.send(bots);
@@ -100,38 +144,44 @@ router.get('/public/templates/:botid', (req, res) => {
       let faqs = null;
       try {
         faqs = await faqService.getAll(id_faq_kb); //.then((faqs) => {
-        // const intents = faqs.map(({_id, id_project, topic, status, id_faq_kb, createdBy, intent_id, createdAt, updatedAt, __v, ...keepAttrs}) => keepAttrs)
-        const intents = faqs.map(({_id, id_project, topic, status, id_faq_kb, createdBy, createdAt, updatedAt, __v, ...keepAttrs}) => keepAttrs)
+        const intents = faqs.map(({ _id, id_project, topic, status, id_faq_kb, createdBy, createdAt, updatedAt, __v, ...keepAttrs }) => keepAttrs)
         let json = {
           webhook_enabled: faq_kb.webhook_enabled,
           webhook_url: faq_kb.webhook_url,
           language: faq_kb.language,
           name: faq_kb.name,
+          title: faq_kb.title,
+          short_description: faq_kb.short_description,
           type: faq_kb.type,
-          description: faq_kb.description,
+          //description: faq_kb.description,
           mainCategory: faq_kb.mainCategory,
           attributes: faq_kb.attributes,
           intents: intents
         }
         return res.send(json);
       }
-      catch(err) {
+      catch (err) {
         console.error('GET FAQ ERROR: ', err)
         return res.status(500).send({ success: false, msg: 'Error getting faqs.' });
       }
     }
     else {
       console.log("private chatbot");
-      res.status(403).send({success: false, message: "Forbidden"});
+      res.status(403).send({ success: false, message: "Forbidden" });
     }
   })
 });
 
+/**
+ * This route serves all the bots metadata for templates/community purposes
+ * 1. import certified templates
+ * 2. import community chatbots
+ */
 router.get('/public/templates/windows/:botid', (req, res) => {
   let id_faq_kb = req.params.botid;
   Faq_kb.findById(id_faq_kb, async (err, faq_kb) => {
     console.log('FAQ-KB: ', faq_kb);
-    console.log('faq_kb.tags: ', faq_kb.tags);
+    //console.log('faq_kb.tags: ', faq_kb.tags);
     if (err) {
       console.error('GET FAQ-KB ERROR ', err)
       return res.status(500).send({ success: false, msg: 'Error getting bot.' });
@@ -142,15 +192,20 @@ router.get('/public/templates/windows/:botid', (req, res) => {
     else if (faq_kb["public"]) {
       let json = {
         _id: faq_kb._id,
+        certified: faq_kb.certified,
         language: faq_kb.language,
         name: faq_kb.name,
         type: faq_kb.type,
         description: faq_kb.description,
         tags: faq_kb.tags,
+        title: faq_kb.title,
+        short_description: faq_kb.short_description,
         bigImage: faq_kb.bigImage,
         mainCategory: faq_kb.mainCategory,
         attributes: faq_kb.attributes,
         templateFeatures: faq_kb.templateFeatures,
+        id_project: faq_kb.id_project,
+        certifiedTags: faq_kb.certifiedTags,
         createdBy: faq_kb.createdBy,
         createdAt: faq_kb.createdAt,
         updatedAt: faq_kb.updatedAt
@@ -159,7 +214,7 @@ router.get('/public/templates/windows/:botid', (req, res) => {
     }
     else {
       console.log("private chatbot");
-      res.status(403).send({success: false, message: "Forbidden"});
+      res.status(403).send({ success: false, message: "Forbidden" });
     }
   })
 });
--- templateRoute/services/faqKbService.js
+++ templateRoute/services/faqKbService.js
@@ -1,4 +1,4 @@
-var Faq_kb = require("./faq_kb");
+var Faq_kb = require("../models/faq_kb");
 
 class FaqKbService {
 
@@ -11,13 +11,15 @@ class FaqKbService {
         "trashed": { $in: [null, false] }
       }
     }
-    var sortQuery = {};
+    var sortQuery = { createdAt: -1 };
     sortQuery["score"] = -1;
     console.log("(Service) GET ALL FAQ_KBs");
     return new Promise((resolve, reject) => {
       // let query = {public: options.public, certified: options.certified};
-      Faq_kb.find(query).sort(sortQuery).lean().exec( (err, bots) => {
+      Faq_kb.find(query).sort(sortQuery).lean().exec((err, bots) => {
+        console.log("sortQuery", sortQuery)
         if (err) {
+          console.error("err", err)
           reject(err);
         }
         resolve(bots);
